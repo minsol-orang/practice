14232e05a13edaf01a44d3d408d7351b191cc8d0
minsol-orang <minsol030507@naver.com>
2026-03-30 20:22:30 +0900

    프렉티스스스스  수정사항 적용

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
           <h1>Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            <p>hello</p>
           </p>
         </div>
         <button
